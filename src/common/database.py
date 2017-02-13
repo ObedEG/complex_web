040e5614ab302a006fe07f83fdb16b54460ef35c
ObedEG <obed.escobedogr@gmail.com>
2017-02-13 14:36:57 -0600

    Fix to default database for mongolab

--- src/common/database.py
+++ src/common/database.py
@@ -11,7 +11,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI, tz_aware=Database.AWARE)
-        Database.DATABASE = client['complex']
+        Database.DATABASE = client.get_default_database()
 
     @staticmethod
     def insert(collection, data):
