a69d606085191da80f310388b413e86e5dfa3137
ObedEG <obed.escobedogr@gmail.com>
2017-02-13 22:32:57 -0600

    Validating MongoDB URI

--- src/common/database.py
+++ src/common/database.py
@@ -10,7 +10,7 @@ class Database(object):
 
     @staticmethod
     def initialize():
-        client = pymongo.MongoClient(Database.URI, tz_aware=Database.AWARE)
+        client = pymongo.MongoClient(Database.URI)
         Database.DATABASE = client.get_default_database()
 
     @staticmethod
