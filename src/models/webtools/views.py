c5de1eb224eb5cff225527103648a6697ac57f0f
ObedEG <obed.escobedogr@gmail.com>
2018-04-13 16:15:25 -0500

    Fix WebTools views, enabling a link to download certain path-mtsn-folder 4

--- src/models/webtools/views.py
+++ src/models/webtools/views.py
@@ -23,7 +23,7 @@ def show_folder():
     return render_template('TEWebtools/get_mtsn.jinja2')
 
 
-@webtool_blueprint.route('/get_mtsn/<string:server><string:path>')
+@webtool_blueprint.route('/download_mtsn/')
 def download_folder(mtsn, path, server):
         if MTSN.copy_folder(mtsn, path, server) == 0:
             try:
