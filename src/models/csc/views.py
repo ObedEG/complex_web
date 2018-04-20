fd99dc4be9bdab9b7c23e48a15aa19657e722866
ObedEG <obed.escobedogr@gmail.com>
2018-04-19 20:45:34 -0500

    Change name Trueven to Truven in al views and functions

--- src/models/csc/views.py
+++ src/models/csc/views.py
@@ -8,13 +8,13 @@ def main_menu():
     return render_template('csc/main_module.jinja2')
 
 
-@csc_blueprint.route('/trueven')
-def trueven_menu():
-    return render_template('csc/trueven_menu.jinja2')
+@csc_blueprint.route('/truven')
+def truven_menu():
+    return render_template('csc/truven_menu.jinja2')
 
-@csc_blueprint.route('/trueven/add_unit', methods=['POST', 'GET'])
-def trueven_addunit():
+@csc_blueprint.route('/truven/add_unit', methods=['POST', 'GET'])
+def truven_addunit():
     if request.method == 'POST':
-        # unit = MTSN(request.form['serial'])
-        return render_template('csc/trueven_addunit.jinja2', server='10.34.70.220')
-    return render_template('csc/trueven_addunit.jinja2')
+        unit = request.form['serial']
+        return render_template('csc/truven_addunit.jinja2', server='10.34.70.220')
+    return render_template('csc/truven_addunit.jinja2')
