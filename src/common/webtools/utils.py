3baf10acd3a08ca307a217af0d62e29e327eab2a
ObedEG <obed.escobedogr@gmail.com>
2018-04-25 12:45:01 -0500

    Fix the truven unit defitinion from csv file

--- src/common/webtools/utils.py
+++ src/common/webtools/utils.py
@@ -14,7 +14,7 @@ class Utils(object):
     def stdout_shell(cmd):
         args = shlex.split(cmd)
         r = subprocess.run(args=args, universal_newlines=False, stdout=subprocess.PIPE)
-        return r.stdout  # return the stdout of the ran cmd!
+        return r.stdout.decode()  # return the stdout of the ran cmd!
 
     @staticmethod
     def truven_def(serial_number):
@@ -22,6 +22,6 @@ class Utils(object):
         keys = ['serial', 'ip-os', 'hostname-xcc', 'ip-xcc', 'subnet-xcc', 'gateway-xcc']
         cmd = 'grep {} /data/CSC/truven/serial_ip.csv'.format(serial_number)
         r = Utils.stdout_shell(cmd)
-        for key, result in keys, r.split(','):
+        for key, result in keys, r.replace('\n', '').split(','):
             truven[key] = result
         return truven
