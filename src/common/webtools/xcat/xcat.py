02a389707488a90dd9c8e53c9e29ea5b4470a1cb
ObedEG <obed.escobedogr@gmail.com>
2018-05-03 11:18:11 -0500

    Try to get a solution to set macs in defined nodes

--- src/common/webtools/xcat/xcat.py
+++ src/common/webtools/xcat/xcat.py
@@ -43,38 +43,31 @@ class Xcat(object):
             Utils.run_shell('ssh ' + vm + ' ' + cmd)
 
     @staticmethod
-    def set_node_switch(hostname, switch, switchport, vm):
-        """
-        Use this function to set switch/switchport definition,
-        in order to test P2P requirements
-        :param vm: Virtual Machine IP - '172.15.0.22'
-        :return: 0 - if the final command ran well
-        """
-        create_node = ' chdef -t node {0} switch={1} switchport={2}'.format(hostname, switch, switchport)
-        return Utils.run_shell('ssh ' + vm + create_node)
-
-    @staticmethod
     def set_node_macs(hostname, macs, vm):
         """
         Use this function to set the MACs registered in xml, so it could be
         discovered by mac without switch/switchport definition
-        :param vm: Virtual Machine IP - '172.15.0.22'
-        :return: 0 - if the final command ran well
         """
-        #  create_node = ' chdef -t node {} mac='.format(hostname) + "'" + macs + "'" < -- Did NOT work!
-
         # Create stanza file of the defined node and add the macs
         cmnds = list()
-        cmnds.append(" lsdef {} > /tmp/{}".format(hostname))
+        cmnds.append(" lsdef {0} > /tmp/{0}".format(hostname))
         cmnds.append(' echo -n "' + "    mac='{}'".format(macs) + '" > /tmp/{}'.format(hostname))
         for cmd in cmnds:
             Utils.run_shell('ssh ' + vm + ' ' + cmd)
-
         # Copy stanza file created to the VM
         copy_stanzafile = "scp /tmp/{0} {1}:/tmp/".format(hostname, vm)
         # Change def of the node using the edited stanza file with macs info
         if Utils.run_shell(copy_stanzafile) == 0:
             return Utils.shell_checkoutput('ssh ' + vm + ' cat /tmp/{} | chdef -z'.format(hostname))
+
+    @staticmethod
+    def set_node_switch(hostname, switch, switchport, vm):
+        """
+        Use this function to set switch/switchport definition,
+        in order to test P2P requirements
+        """
+        create_node = ' chdef -t node {0} switch={1} switchport={2}'.format(hostname, switch, switchport)
+        return Utils.run_shell('ssh ' + vm + create_node)
 
     @staticmethod
     def restart_discovery_services(vm):
