e14d3ee3836a71e75497af09714d3da9e1b41345
ObedEG <obed.escobedogr@gmail.com>
2018-05-18 19:38:36 -0500

    Try to get a solution

--- src/common/webtools/csc/truven_utils.py
+++ src/common/webtools/csc/truven_utils.py
@@ -138,7 +138,13 @@ class TruvenUtils(object):
 
     @staticmethod
     def run_test(unit):
-        return TruvenUtils.copy_asu_to_unit(vm=csc_truven_vm, sn=unit.ip_os)
+        if TruvenUtils.copy_asu_to_unit(vm=csc_truven_vm, sn=unit.ip_os) == 0:
+            if TruvenUtils.verify_unit_tools(vm=csc_truven_vm, sn=unit.sn) == 0:
+                hostname = TruvenUtils.change_xcc_hostname(vm=csc_truven_vm, sn=unit.sn, xcc_hostname=unit.hostname)
+                netmask = TruvenUtils.change_xcc_netmask(vm=csc_truven_vm, sn=unit.sn, xcc_netmask=unit.netmask)
+                gateway = TruvenUtils.change_xcc_gateway(vm=csc_truven_vm, sn=unit.sn, xcc_gateway=unit.gateway)
+                if hostname + netmask + gateway == 0:
+                    return "We finished the test!!!"
 
     """Pending Gathering info
 
